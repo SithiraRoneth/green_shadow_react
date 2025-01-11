ed89ecd850fab4cf0899c68b5f5ada1a32b3d330
USER <sithiraroneth@gmail.com>
2025-01-11 22:26:13 +0530

    add equipmentReducer

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -3,12 +3,14 @@ import cropReducer from '../reducers/CropSlice.ts'
 import fieldReducer from '../reducers/FieldSlice.ts'
 import vehicleReducer from '../reducers/VehicleSlice.ts'
 import staffReducer from '../reducers/StaffSlice.ts'
+import equipSlice from '../reducers/EquipmentSlice.ts'
 export const Store = configureStore({
     reducer: {
         crops: cropReducer,
         fields: fieldReducer,
         vehicles: vehicleReducer,
         staffs: staffReducer,
+        equipments: equipSlice,
     }
 })
 
