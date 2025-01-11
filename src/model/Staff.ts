f7c5af599896dbabfa7379344801dfafe4f1bf90
USER <sithiraroneth@gmail.com>
2025-01-11 22:12:43 +0530

    add Staff.ts

---
+++ src/model/Staff.ts
@@ -0,0 +1,15 @@
+export class Staff {
+    staffId:string;
+    firstName:string;
+    lastName:string;
+    email:string;
+    phone:number;
+
+    constructor(staffId:string,firstName:string,lastName:string,email:string,phone:number){
+        this.staffId = staffId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+}
