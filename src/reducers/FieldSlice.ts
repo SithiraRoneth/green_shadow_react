e049a2a1bd6d4b0b31eaa147ed4d6e223d95e59c
USER <sithiraroneth@gmail.com>
2025-01-11 22:12:12 +0530

    edit FieldSlice.ts

--- src/reducers/FieldSlice.ts
+++ src/reducers/FieldSlice.ts
@@ -5,8 +5,24 @@ const FieldSlice = createSlice({
     name:'fields',
     initialState:initialState,
     reducers:{
-
+        addField:(state,action)=>{
+            console.log("Data fetch")
+            state.push(action.payload);
+        },
+        updateField:(state,action)=>{
+            const index = state.findIndex(field => field.fieldCode === action.payload.fieldCode);
+            if (index !== -1){
+                state[index] ={
+                    ...state[index],
+                    ...action.payload,
+                };
+            }
+        },
+        deleteField:(state,action)=>{
+            return state.filter(field => field.fieldCode !== action.payload.fieldCode);
+        }
     }
 })
 
+export const {addField, updateField,deleteField} = FieldSlice.actions;
 export default FieldSlice.reducer
