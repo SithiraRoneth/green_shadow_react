a0fcc0257181029e812141200d7db663013f9c32
USER <sithiraroneth@gmail.com>
2025-01-11 22:13:06 +0530

    add VehicleSlice.ts

--- src/reducers/VehicleSlice.ts
+++ src/reducers/VehicleSlice.ts
@@ -5,8 +5,24 @@ const VehicleSlice = createSlice({
     name : 'vehicles',
     initialState : initialState,
     reducers : {
-
+        addVehicle : (state, action) => {
+            console.log('Data fetch')
+            state.push(action.payload);
+        },
+        updateVehicle : (state, action) => {
+            const index = state.findIndex(vehicle => vehicle.licensePlate === action.payload.licensePlate);
+            if(index !== -1) {
+                state[index] = {
+                    ...state[index],
+                    ...action.payload,
+                };
+            }
+        },
+        deleteVehicle : (state, action) => {
+            return state.filter(vehicle => vehicle.licensePlate !== action.payload.licensePlate);
+        }
     }
 })
 
+export const {addVehicle,updateVehicle,deleteVehicle} = VehicleSlice.actions;
 export default VehicleSlice.reducer;
