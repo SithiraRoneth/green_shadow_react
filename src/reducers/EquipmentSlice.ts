1fcaa651e385ab86237f920b240f7724ccb0ed46
USER <sithiraroneth@gmail.com>
2025-01-17 23:16:32 +0530

    change initialState

--- src/reducers/EquipmentSlice.ts
+++ src/reducers/EquipmentSlice.ts
@@ -3,7 +3,7 @@ import {createSlice} from "@reduxjs/toolkit";
 const initialState = []
 const EquipmentSlice = createSlice({
     name : 'equips',
-    initialState,
+    initialState : initialState,
     reducers: {
         addEquipment: (state, action) => {
             console.log("Data fetch")
