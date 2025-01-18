14d99b3e2fc01747f897ee17421e3761b2372df4
USER <sithiraroneth@gmail.com>
2025-01-18 22:39:05 +0530

    add staff form and some logics

--- src/model/Staff.ts
+++ src/model/Staff.ts
@@ -2,13 +2,15 @@ export class Staff {
     staffId:string;
     firstName:string;
     lastName:string;
+    jobName:string;
     email:string;
     phone:number;
 
-    constructor(staffId:string,firstName:string,lastName:string,email:string,phone:number){
+    constructor(staffId:string,firstName:string,lastName:string,jobName:string,email:string,phone:number){
         this.staffId = staffId;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.jobName = jobName;
         this.email = email;
         this.phone = phone;
     }
