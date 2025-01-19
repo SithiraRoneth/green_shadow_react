dada9688996ab9c7862de4c6ac45e0d5f31ee7e0
USER <sithiraroneth@gmail.com>
2025-01-19 23:15:31 +0530

    Add EquipmentTable as a component

---
+++ src/components/Equipment/EquipmentTable.tsx
@@ -0,0 +1,25 @@
+import {CircleMinus} from "lucide-react";
+
+export default function EquipmentTable({equip,onUpdate,onDelete,index}){
+    return (
+        <li
+            className={`grid grid-cols-4 items-center text-center p-2 border rounded-full hover:bg-gray-100 ${index % 2 === 0 ? "bg-gradient-to-r from-slate-500 to-slate-50" : "bg-white"}`}
+            onClick={() => onUpdate(equip)}
+        >
+            <span>{equip.equipId}</span>
+            <span>{equip.equipName}</span>
+            <span>{equip.equipType}</span>
+
+            <button
+                onClick={(e) => {
+                    e.stopPropagation();
+                    onDelete(equip.equipId);
+                }}
+                className="text-red-500 hover:text-red-700 flex justify-center"
+                title="Delete Equipment"
+            >
+                <CircleMinus className="w-5 h-5"/>
+            </button>
+        </li>
+    )
+}
